9cb366e72afbfef3b4d7dcdb46027897cc75809b
LIU HUAN <huan.liu>
2021-06-22 17:14:22 +0800

    [update] zipkin client

--- test/zipkinClientHttp/go.mod
+++ test/zipkinClientHttp/go.mod
@@ -4,6 +4,9 @@ go 1.16
 
 require (
 	contrib.go.opencensus.io/exporter/zipkin v0.1.2 // indirect
+	github.com/gin-gonic/gin v1.7.2 // indirect
 	github.com/openzipkin/zipkin-go v0.2.5 // indirect
 	go.opencensus.io v0.23.0 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+	gopkg.in/go-playground/validator.v9 v9.29.1 // indirect
 )
--- test/zipkinClientHttp/zipkin_client_test.go
+++ test/zipkinClientHttp/zipkin_client_test.go
@@ -1,12 +1,18 @@
 package zipkinClientHttp
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"testing"
 	"time"
+
+	"github.com/gin-gonic/gin"
+	// "go.opencensus.io/plugin/ochttp"
+	// "go.opencensus.io/stats/view"
+	"go.opencensus.io/trace"
 )
 
 func TestClient(t *testing.T) {
@@ -37,4 +43,48 @@ func TestClient(t *testing.T) {
 
 	// Output:
 	log.Printf("result %s", body)
+}
+
+func TestServer(t *testing.T) {
+	Init(WithLocalServerName("go-test-svr"), WithServiceName("local_svr_name"), WithLocalAddr("192.168.0.2"))
+	defer Destroy()
+	rgin := gin.Default()
+	// 第三步: 添加一个 middleWare, 为每一个请求添加span
+	// r.Use(zipkinMiddle)
+	rgin.GET("/",
+		func(c *gin.Context) {
+			time.Sleep(500 * time.Millisecond)
+			c.JSON(200, gin.H{"code": 200, "msg": "OK-root"})
+		})
+	rgin.GET("/list",
+		func(c *gin.Context) {
+			// time.Sleep(500 * time.Millisecond)
+			dohttp(c)
+			c.JSON(200, gin.H{"code": 200, "msg": "OK-list"})
+		})
+	RunGinServer(rgin, 8080)
+	// h := &ochttp.Handler{Handler: rgin}
+	// if err := view.Register(ochttp.DefaultServerViews...); err != nil {
+	// 	log.Fatal("Failed to register ochttp.DefaultServerViews")
+	// }
+	// http.Handle("/", h)
+	// port := ":8080"
+	// log.Printf("listen:%s", port)
+	// if err := http.ListenAndServe(port, nil); err != nil {
+	// 	log.Fatalf("err:%s", err)
+	// }
+}
+func dohttp(c *gin.Context) {
+	//span := r.zkTracer.StartSpan("dohttp")
+	_, span := trace.StartSpan(c.Request.Context(), "dohttp")
+	defer span.End()
+	doOther(c)
+	time.Sleep(400 * time.Millisecond)
+}
+func doOther(c context.Context) {
+	_, span := trace.StartSpan(c, "do_other")
+	defer span.End()
+	log.Println("do_other")
+	time.Sleep(100 * time.Millisecond)
+
 }
